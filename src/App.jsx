535d6f99ba83fdaf20c2db1c09cafb87d744e09f
akwinder <akwinder.kaur@sifars.com>
2026-03-11 13:29:55 +0530

    mui-material setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
 
   return (
     <>
       <h1 class="text-3xl font-bold underline">hello</h1>
+      <Button variant="contained">Click</Button>
     </>
   )
 }
