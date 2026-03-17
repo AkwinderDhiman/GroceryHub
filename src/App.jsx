f9d37ff3f6d36b7c2915257acbc67339e4b3870c
akwinder <akwinder.kaur@sifars.com>
2026-03-17 14:25:04 +0530

    home route set

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Button from '@mui/material/Button';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home';
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">hello</h1>
-      <Button variant="contained">Click</Button>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact={true} element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
