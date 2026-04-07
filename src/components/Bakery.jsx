c7836728972c149c9c074f2b21a1cf9ee8efab1e
akwinder <akwinder.kaur@sifars.com>
2026-04-07 17:44:37 +0530

    fix: update grid layout for responsive design

--- src/components/Bakery.jsx
+++ src/components/Bakery.jsx
@@ -66,7 +66,7 @@ export default function Bakery() {
         </div>
 
         {/* Grid */}
-        <div className="grid grid-cols-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {bakeryItems.map((item) => (
             <div
               key={item.id}
