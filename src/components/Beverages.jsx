cd9b23fafec7b3a34925d682ed5ee28f2f869c56
akwinder <akwinder.kaur@sifars.com>
2026-04-21 16:42:45 +0530

    feat : product Id update

--- src/components/Beverages.jsx
+++ src/components/Beverages.jsx
@@ -4,37 +4,37 @@ import ProductCard from "./ProductCard";
 export default function Beverages() {
   const drinks = [
     {
-      id: 1,
+      id: 21,
       name: "Fresh Orange Juice",
       price: "$4.99",
       image: "https://images.unsplash.com/photo-1600271886742-f049cd451bba",
     },
     {
-      id: 2,
+      id: 22,
       name: "Cold Coffee",
       price: "$3.49",
       image: "https://images.unsplash.com/photo-1509042239860-f550ce710b93",
     },
     {
-      id: 3,
+      id: 23,
       name: "Green Smoothie",
       price: "$5.99",
       image: "https://images.unsplash.com/photo-1553530666-ba11a7da3888",
     },
     {
-      id: 4,
+      id: 24,
       name: "Strawberry Shake",
       price: "$4.49",
       image: "https://images.unsplash.com/photo-1497534446932-c925b458314e",
     },
     {
-      id: 5,
+      id: 25,
       name: "Lemonade",
       price: "$2.99",
       image: "https://images.unsplash.com/photo-1524594154908-edd1b1d5d06d",
     },
     {
-      id: 6,
+      id: 26,
       name: "Iced Tea",
       price: "$3.29",
       image: "https://images.unsplash.com/photo-1498804103079-a6351b050096",
